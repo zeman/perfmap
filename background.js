1e5e170e189a2e0a8499546d7171f37b2dce18e6
Umar <umar.hansa@gmail.com>
2014-10-14 12:51:48 +0100

    Use remote script

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-    chrome.tabs.executeScript(null, {file: "perfmap.js"});
+    chrome.tabs.executeScript(null, {
+    	code: "javascript:(function(){var el=document.createElement('script');el.src='https://zeman.github.io/perfmap/perfmap.js';document.body.appendChild(el);})();"
+    });
 });
