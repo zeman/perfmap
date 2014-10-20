c64796b9536032b4ca35433a59cad304ec822128
Leo Deng <myst729@users.noreply.github.com>
2014-10-20 11:10:26 +0800

    Fix mixed indent

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
     chrome.tabs.executeScript(null, {
-    	code: "javascript:(function(){var el=document.createElement('script');el.src='https://zeman.github.io/perfmap/perfmap.js';document.body.appendChild(el);})();"
+        code: "javascript:(function(){var el=document.createElement('script');el.src='https://zeman.github.io/perfmap/perfmap.js';document.body.appendChild(el);})();"
     });
 });
